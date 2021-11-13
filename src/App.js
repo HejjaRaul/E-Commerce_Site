c980e58996f330684a11549d68ce5098b4c169db
HejjaRaul <raul.hejja@student.upt.ro>
2021-11-13 20:30:32 +0200

    Added the log in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import HomePageIndex from "./Pages/Home/HomePageIndex";
+import LoginIndex from "./Pages/Login/LoginIndex";
 import ProductsListPageIndex from "./Pages/ProductsList/ProductsListPageIndex";
 import RegisterIndex from "./Pages/Register/RegisterIndex";
 import ProductPageIndex from "./Pages/SingleProduct/ProductPageIndex";
 
 function App() {
-  return <RegisterIndex />;
+  return <LoginIndex />;
 }
 
 export default App;
