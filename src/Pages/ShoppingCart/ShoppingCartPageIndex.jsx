4ed1398a84a7cb9d5ab5eb35838a244fa0ca40e3
HejjaRaul <raul.hejja@student.upt.ro>
2021-11-21 14:07:07 +0200

    Added the top part of the shopping cart including the buttons for checkout and continue shopping.

--- src/Pages/ShoppingCart/ShoppingCartPageIndex.jsx
+++ src/Pages/ShoppingCart/ShoppingCartPageIndex.jsx
@@ -2,15 +2,71 @@ import styled from "styled-components";
 import AnnouncementsIndex from "./Components/AnnouncementsIndex";
 import TopBarIndex from "./Components/TopBarIndex";
 
+//<---------------------------------------Start of CSS - styling------------------------------------------------>
+
 const Container = styled.div`
   /* ... */
 `;
 
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${props => props.type === "filled" && "none"};
+  background-color: ${props => props.type === "filled" ? "black" : "transparent"};
+  color: ${props => props.type === "filled" && "white"};
+`
+const TopTexts = styled.div``
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0 10px;
+`
+
+const Bottom = styled.div`
+  /* ... */
+`;
+
+//<---------------------------------------End of CSS - styling------------------------------------------------>
+
+//<---------------------------------------Start of HTML - coding---------------------------------------------->
+
 export default function ShoppingCartPageIndex() {
-  return (
-    <Container>
-      <TopBarIndex />
-      <AnnouncementsIndex />
-    </Container>
-  );
+    return (
+        <Container>
+            <TopBarIndex/>
+            <AnnouncementsIndex/>
+            <Wrapper>
+                <Title>YOUR CART</Title>
+                <Top>
+                    <TopButton>CONTINUE SHOPPING</TopButton>
+                    <TopTexts>
+                        <TopText>Shopping Cart(2)</TopText>
+                        <TopText>Your Wishlist(0)</TopText>
+                    </TopTexts>
+                    <TopButton type="filled">CHECKOUT NOW</TopButton>
+                </Top>
+                <Bottom></Bottom>
+            </Wrapper>
+        </Container>
+    );
 }
+
+//<---------------------------------------End of HTML - coding---------------------------------------------->
