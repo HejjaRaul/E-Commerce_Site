41dd750d2e6534bb6f8d632217bee9e0666f3080
HejjaRaul <raul.hejja@student.upt.ro>
2021-11-21 14:47:06 +0200

    Added the product image and details

--- src/Pages/ShoppingCart/ShoppingCartPageIndex.jsx
+++ src/Pages/ShoppingCart/ShoppingCartPageIndex.jsx
@@ -41,8 +41,55 @@ const TopText = styled.span`
 `
 
 const Bottom = styled.div`
-  /* ... */
+  display: flex;
+  justify-content: space-between;
 `;
+
+const Info = styled.div`
+  flex: 3;
+`
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`
+const Image = styled.img`
+  width: 200px;
+  height: 300px;
+`
+const Details = styled.div`
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+const ProductName = styled.span`
+  font-size: 20px;
+`
+const ProductId = styled.span`
+  font-size: 20px;
+`
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+`
+const ProductSize = styled.span`
+  font-size: 20px;
+`
+const PriceDetail = styled.div`
+  flex: 1;
+`
+
+
+const Summary = styled.div`
+  flex: 1;
+`
 
 //<---------------------------------------End of CSS - styling------------------------------------------------>
 
@@ -63,7 +110,24 @@ export default function ShoppingCartPageIndex() {
                     </TopTexts>
                     <TopButton type="filled">CHECKOUT NOW</TopButton>
                 </Top>
-                <Bottom></Bottom>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image
+                                    src="https://i.pinimg.com/originals/35/1c/76/351c76792125bf3dd0820bcf5828e9c0.jpg"/>
+                                <Details>
+                                    <ProductName><b>Product:</b> BLACK JEANS</ProductName>
+                                    <ProductId><b>ID:</b> 9042152</ProductId>
+                                    <ProductColor color="black"/>
+                                    <ProductSize><b>Size:</b> M</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>price</PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>summary</Summary>
+                </Bottom>
             </Wrapper>
         </Container>
     );
