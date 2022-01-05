2d4c20236b4ff060ec6feb3734b3037e8a8024e8
HejjaRaul <raul.hejja@student.upt.ro>
2022-01-05 19:49:13 +0200

    Added token verification and routes for changing username

--- src/API/Routes/UserRoute.js
+++ src/API/Routes/UserRoute.js
@@ -1,3 +1,24 @@
+const {verifyToken, verifyTokenAndAuthorization} = require("./VerifyToken");
+const CryptoJS = require("crypto-js");
 const router = require("express").Router();
+const UserModel = require("../Models/UserModel");
+
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECURE_KEY).toString();
+    }
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new:true});
+        res.status(200).json(updatedUser);
+    }
+    catch(err)
+    {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
